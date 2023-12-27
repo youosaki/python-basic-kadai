439136e8ea32140bea189108135d38075997d9ed
youosaki <154046805+youosaki@users.noreply.github.com>
2023-12-27 12:47:59 +0900

    kadai_008.pyを更新　elifに記載変更

--- kadai_008/kadai_008.py
+++ kadai_008/kadai_008.py
@@ -1,19 +1,22 @@
 def FizzBuzz(var):
 
+  #変数varが、3の倍数と5の倍数の両方に該当する場合は「FizzBuzz」を出力
+  if var % 3 == 0 and var % 5 == 0:
+    print("FizzBuzz", end = "")
+
   #変数varが、3の倍数の場合は「Fizz」を出力
-  if var % 3 == 0:
+  elif var % 3 == 0:
     print("Fizz", end = "")
   
   #変数varが、5の倍数の場合は「Buzz」を出力
-  #変数varが、3の倍数と5の倍数の両方に該当する場合は「FizzBuzz」を出力
-  if var % 5 == 0:
+  elif var % 5 == 0:
     print("Buzz", end = "")
 
   #上記のどの場合にも該当しない場合は、変数varの値を出力
-  if var % 3 != 0 and var % 5 != 0:
+  else:
     print(var)
 
   print("")
 
-var = 3
+var = 15
 FizzBuzz(var)
